a278c7b991526b0f9fda06700e27016eded7279a
Toru Ishihara <toru148@gmail.com>
2026-05-22 18:16:00 +0900

    Small fixes

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import subprocess
 import time
 import shutil
 
-SAVE_CNT = 1000
+SAVE_CNT = 5000
 LORA_LAST_SENT = 0
 LORA_COOLDOWN_SECONDS = 60
 USB_SAVE_DIR = "/mnt/kioxia"
 USB_SAVE_DIR2 = "/mnt/kioxia1"
 LOCAL_SAVE_DIR = "frames"
 IGNORE_CLASSES = {"car", "truck", "bus", "person", "umbrella"}
-LOCAL_LOG_FILE = "/home/toru/JetsonNano-YOLOv5-Animal-Detector/main.log"
+LOCAL_LOG_FILE = "/tmp/main.log"
 
 def copy_main_log_to_usb():
     usb_dirs = [
@@ -23,20 +23,23 @@ def copy_main_log_to_usb():
     if not os.path.isfile(LOCAL_LOG_FILE):
         print("ERROR: local main.log does not exist:", LOCAL_LOG_FILE)
         return False
+
     for usb_dir in usb_dirs:
         if not usb_dir:
             continue
         if not os.path.isdir(usb_dir):
             print("USB dir not found:", usb_dir)
             continue
+
         dest_log_file = os.path.join(usb_dir, "main.log")
+
         try:
             shutil.copy2(LOCAL_LOG_FILE, dest_log_file)
             print("Copied main.log to:", dest_log_file)
             return True
         except Exception as e:
-            print("Failed to copy main.log to:", dest_log_file)
-            print("Reason:", e)
+            pass
+
     print("ERROR: failed to copy main.log to both USB_SAVE_DIR and USB_SAVE_DIR2")
     return False
 
@@ -126,18 +129,31 @@ while True:
     if save_frame:
         results.render()
         img = results.imgs[0]
-        now = datetime.now()
-        ts = now.strftime("%m%d_%H%M%S") + f"_{now.microsecond // 100000}"
 
-        if os.path.isdir(USB_SAVE_DIR):
-            save_dir = f"{USB_SAVE_DIR}/frames"
-        elif os.path.isdir(USB_SAVE_DIR2):
-            save_dir = f"{USB_SAVE_DIR2}/frames"
+        now = datetime.now()
+        ts = now.strftime("%m%d_%H%M%S") + f"_{now.microsecond // 10000}"
+
+        save_dir1 = f"{USB_SAVE_DIR}/frames"
+        save_dir2 = f"{USB_SAVE_DIR2}/frames"
+        local_dir = f"{LOCAL_SAVE_DIR}/frames"
+
+        # Choose save directory
+        if os.path.isdir(save_dir1):
+            save_dir = save_dir1
+        elif os.path.isdir(save_dir2):
+            save_dir = save_dir2
         else:
-            save_dir = LOCAL_SAVE_DIR
+            save_dir = local_dir
+            os.makedirs(save_dir, exist_ok=True)
+
         filename = f"{save_dir}/frame_{ts}.jpg"
-        cv2.imwrite(filename, img)
-        print("saved:", filename)
+        ok = cv2.imwrite(filename, img)
+
+        if ok:
+            print("saved:", filename)
+        else:
+            print("ERROR: failed to save:", filename)
+            send_lora_alert(f"failed to save:{filename}")
 
     # Press q to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -150,8 +166,10 @@ while True:
         diff = now - start_lap
 
         ts = datetime.now().strftime("%H%M%S")
-        print("ts=", ts, " cnt=", cnt, " fps=", SAVE_CNT/diff) 
+        fps = SAVE_CNT/diff
+        print("ts=", ts, " cnt=", cnt, " fps=", fps, flush=True) 
         copy_main_log_to_usb()
+        send_lora_alert(f"{ts} fps={fps}")
         start_lap = now
 
 cap.release()
